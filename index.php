9930ecf3308b8c7ca1263c0c606161e739a651da
nghiatui2233 <91666125+nghiatui2233@users.noreply.github.com>
2021-10-08 17:06:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,89 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+
+<html>
+
+<title>ZOLMKOZ</title>
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+
+<style>
+
+    body,
+
+    h1 {
+
+        font-family: "Raleway", sans-serif
+
+    }
+
+
+
+    body,
+
+    html {
+
+        height: 98%
+
+    }
+
+
+
+    .bgimg {
+
+        background-image: url('http://trumpwallpapers.com/wp-content/uploads/Meme-Wallpaper-21-1920-x-1080.jpg');
+
+        min-height: 98%;
+
+        background-position: center;
+
+        background-size: cover;
+
+    }
+
+</style>
+
+
+
 <body>
-    <?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
-?>
+
+
+
+    <div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
+
+        <div class="w3-display-topleft w3-padding-large w3-xlarge">
+
+            ZOLMKOZ
+
+        </div>
+
+        <div class="w3-display-middle">
+
+            <h1 class="w3-jumbo w3-animate-top">COMING SOON</h1>
+
+            <hr class="w3-border-grey" style="margin:auto;width:40%">
+
+            <p class="w3-large w3-center">35 days left</p>
+
+        </div>
+
+        <div class="w3-display-bottomleft w3-padding-large">
+
+            Powered by <a href="https://www.facebook.com/zolmkoz.official" target="_blank">ZOLMKOZ</a>
+
+        </div>
+
+    </div>
+
+
+
 </body>
+
+
+
 </html>
